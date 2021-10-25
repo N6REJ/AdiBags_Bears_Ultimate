28b74848a310a25d2561ee749e4360bef42e756d
N6REJ <programming@hallhome.us>
2021-10-25 17:01:05 -0500

    create filter check #1

--- core.lua
+++ core.lua
@@ -1,87 +1,105 @@
--- AdiBags_Shadowlands_Crafting
+-- AdiBags_Shadowlands_Fishing
 -- Created by N6REJ character is Bearesquishy - dalaran please credit whenever.
--- Source on GitHub: https://github.com/N6REJ/AdiBags_Shadowlands_Crafting
+-- Source on GitHub: https://github.com/N6REJ/Adibags_Shadowlands_Fishing
 
 
-local ADDON_NAME, ADDON_TABLE, addon = ...
+local addonName, addonTable, addon = ...
 
 -- Get reference to AdiBags addon
 local AdiBags = LibStub("AceAddon-3.0"):GetAddon("AdiBags")
 
--- Addon info
-local version = GetAddOnMetadata(ADDON_NAME, "Version");
-local addoninfo = 'Shadowlands Crafting - ' .. N["FilterTitle"];
+-- Debug tools
+--LoadAddOn("ViragDevTool")
+--ViragDevTool:ViragDevTool_AddData(addonTable)
 
-local N = ADDON_TABLE.N
+local db = addonTable.db
 local MatchIDs
-local Tooltip
-local Result = {}
+local tooltip
+local Result = { }
 
--- Register this addon with AdiBags
-local setFilter = AdiBags:RegisterFilter(ADDON_NAME, 100, "ABEvent-1.0")
-setFilter.uiName = addoninfo
-setFilter.uiDesc = "Puts base " .. N["FilterTitle"] .. " crafting mats from drops or farming in their own group" .. "     Version: " .. version
-
-
-local function AddToSet(Set, List)
-	for _, v in ipairs(List) do
-		Set[v] = true
-	end
-end
-
-local function MatchIDs_Init(self)
-	wipe(Result)
-
-	AddToSet(Result, N["database"])
-
-	return Result
-end
-
-local function Tooltip_Init()
+local function tooltipInit()
 	local tip, leftside = CreateFrame("GameTooltip"), {}
 	for i = 1, 6 do
-		local Left, Right = tip:CreateFontString(), tip:CreateFontString()
-		Left:SetFontObject(GameFontNormal)
-		Right:SetFontObject(GameFontNormal)
-		tip:AddFontStrings(Left, Right)
-		leftside[i] = Left
+		local left, right = tip:CreateFontString(), tip:CreateFontString()
+		left:SetFontObject(GameFontNormal)
+		right:SetFontObject(GameFontNormal)
+		tip:AddFontStrings(left, right)
+		leftside[i] = left
 	end
 	tip.leftside = leftside
 	return tip
 end
 
-function setFilter:OnInitialize()
-	self.db = AdiBags.db:RegisterNamespace(ADDON_NAME)
-end
+-- Create Filters
+local function CreateFilter(name, uiName, uiDesc, title, items)
 
-function setFilter:Update()
-	MatchIDs = nil
-	self:SendMessage("AdiBags_FiltersChanged")
-end
+	-- Register Filter with adibags
+	local filter = AdiBags:RegisterFilter(uiName, 98, "ABEvent-1.0")
 
-function setFilter:OnEnable()
-	AdiBags:UpdateFilters()
-end
+	filter.uiName = uiName
+	filter.uiDesc = uiDesc .. "Version:" .. GetAddOnMetadata(addonName, "Version");
+	filter.items = items
 
-function setFilter:OnDisable()
-	AdiBags:UpdateFilters()
-end
-
-function setFilter:Filter(slotData)
-	MatchIDs = MatchIDs or MatchIDs_Init(self)
-	if MatchIDs[slotData.itemId] then
-		return N["FilterTitle"]
+	function filter:OnInitialize()
+		-- Assign item table to filter
+		self.items = filter.items
 	end
 
-	Tooltip = Tooltip or Tooltip_Init()
-	Tooltip:SetOwner(UIParent,"ANCHOR_NONE")
-	Tooltip:ClearLines()
-
-	if slotData.bag == BANK_CONTAINER then
-		Tooltip:SetInventoryItem("player", BankButtonIDToInvSlotID(slotData.slot, nil))
-	else
-		Tooltip:SetBagItem(slotData.bag, slotData.slot)
+	function filter:Update()
+		self:SendMessage("AdiBags_FiltersChanged")
 	end
 
-	Tooltip:Hide()
+	function filter:OnEnable()
+		AdiBags:UpdateFilters()
+	end
+
+	function filter:OnDisable()
+		AdiBags:UpdateFilters()
+	end
+
+	function filter:Filter(slotData)
+		if self.items[tonumber(slotData.itemId)] then
+			return title
+		end
+
+		tooltip = tooltip or tooltipInit()
+		tooltip:SetOwner(UIParent, "ANCHOR_NONE")
+		tooltip:ClearLines()
+
+		if slotData.bag == BANK_CONTAINER then
+			tooltip:SetInventoryItem("player", BankButtonIDToInvSlotID(slotData.slot, nil))
+		else
+			tooltip:SetBagItem(slotData.bag, slotData.slot)
+		end
+
+		tooltip:Hide()
+	end
 end
+
+-- Run filters
+local function AllFilters(db)
+	for name, group in pairs(db.Filters) do
+
+		-- Does filter already exist?
+		local FilterExists = CheckFilter(group.uiName)
+		print(FilterExists)
+
+
+		-- name = Name of table
+		-- group.uiName = Name to use in filter listing
+		-- group.uiDesc = Description to show in filter listing
+		-- group.items = table of items to sort
+		CreateFilter(name, group.uiName, group.uiDesc, group.title, group.items)
+    end
+end
+
+local function CheckFilter(FilterExists)
+	for i, filter in AdiBags:IterateFilters() do
+		if filter == FilterExists then
+			return true
+		end
+	end
+end
+
+-- Start here
+AllFilters(db)
