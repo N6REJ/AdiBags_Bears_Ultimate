79b621876882ed9ad3a92cbae025fcb74ab0cc66
N6REJ <programming@hallhome.us>
2021-10-31 06:38:42 -0500

    changed header in db to be more agnostic

--- database.lua
+++ database.lua
@@ -1,6 +1,6 @@
 -- AdiBags_Shadowlands_Cooking - Database
 -- Created by N6REJ character is Bearesquishy - dalaran please credit whenever.
--- Source on GitHub: https://github.com/N6REJ/Adibags_Shadowlands_Fishing
+-- Source on GitHub: https://n6rej.github.io
 --
 local addonName, addonTable, addon = ...
 
