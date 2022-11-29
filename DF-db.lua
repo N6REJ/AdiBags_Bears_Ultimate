0cf4d47aaead0e2dc2855621ed84b5347c6acb21
Bear <programming@hallhome.us>
2022-11-28 20:12:21 -0600

    Added a few elementals

--- DF-db.lua
+++ DF-db.lua
@@ -69,6 +69,23 @@ db.Filters = {
 			-- ID  = true,		--Item name
 			[171841] = true -- Shaded Stone - [ Required for kettle of stone soup ]
 		}
+	},
+	["Elemental"] = {
+		uiName = Expansion .. " Elemental",
+		uiDesc = "Elemental reagents found in " .. Expansion,
+		title = "Elemental",
+		items = {
+			-- ID  = true,		--Item name
+			[190326] = true, -- Rousing Air
+			[190315] = true, -- Rousing Earth
+			[190320] = true, -- Rousing Fire
+			[190451] = true, -- Rousing Ire
+			[190328] = true, -- Rousing Frost
+			[190321] = true, -- Awakened Fire
+			[190327] = true, -- Awakened Air
+			[190329] = true, -- Awakened Frost
+			[190316] = true, -- Awakened Fire
+		}
 	}
 }
 
