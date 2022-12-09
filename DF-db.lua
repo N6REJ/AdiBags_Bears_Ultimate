0e0b129f0d700c018f895843cf2e366c4c8f6cf8
Bear <programming@hallhome.us>
2022-12-09 02:18:08 -0600

    removed universal items in favor of global files. fixed syntax error

--- DF-db.lua
+++ DF-db.lua
@@ -17,18 +17,6 @@ db.desc = "Items & Reagents found in " .. Expansion
 
 -- Filter info
 db.Filters = {
-	["Vendor_Reagents"] = {
-		uiName = Expansion .. " Vendor Reagents",
-		uiDesc = "Reagents from vendors used in " .. Expansion,
-		title = "Vendor",
-		items = {
-			-- ID  = true,		--Item name
-
-			-- Inscription
-			[194784] = true, -- Glittering Parchment
-			[197752] = true, -- Conveniently Packaged Ingredients
-		}
-	},
  ["Ore"] = {
 		uiName = Expansion .. " Mining",
 		uiDesc = "Ore found in " .. Expansion,
@@ -130,27 +118,6 @@ db.Filters = {
 			[197772] = true, -- CHurnbelly Tea
 		}
 	},
---[[	["Elemental"] = {
-		uiName = Expansion .. " Elemental",
-		uiDesc = "Elemental reagents found in " .. Expansion,
-		title = "Elemental",
-		items = {
-			-- ID  = true,		--Item name
-			[190326] = true, -- Rousing Air
-			[190315] = true, -- Rousing Earth
-			[190320] = true, -- Rousing Fire
-			[190451] = true, -- Rousing Ire
-			[190328] = true, -- Rousing Frost
-			[190322] = true, -- Rousing Order
-			[190330] = true, -- Rousing Decay
-			[190331] = true, -- Awakened Decay
-			[190321] = true, -- Awakened Fire
-			[190327] = true, -- Awakened Air
-			[190329] = true, -- Awakened Frost
-			[190316] = true, -- Awakened Fire
-			[190324] = true, -- Awakened Order
-		},
---]]
 	["Crafted_Reagents"] = {
 		uiName = Expansion .. " Crafted Reagents",
 		uiDesc = "Assorted crafted reagents used in " .. Expansion,
@@ -167,22 +134,6 @@ db.Filters = {
 			[194727] = true,    -- Fiery Spirit 2
 			[194729] = true,    -- Fiery Spirit 3
 			[197757] = true, -- Assorted Exotic Spices
-		}
-	},
-	["Zone_Reagents"] = {
-		uiName = "Special Reagents",
-		uiDesc = "Special reagents from " .. Expansion,
-		title = "DF Mats",
-		items = {
-			-- ID  = true,		--Item name
-			[190454] = true, -- Primal Chaos
-			[202122] = true, -- Primal Chaos Cluster
-			[200686] = true, -- Primal Focus
-			[190456] = true, -- Artisan's Mettle
-			[201405] = true, -- Tuft of Primal Wool -- Used by many professions
-			[201406] = true, -- Glowing Titan Orb   -- Used by many professions
-			[190453] = true, -- Spark of Ingenuity
-			[199197] = true, -- Bottled Essence
 		}
 	},
 	["Knowledge"] = {
