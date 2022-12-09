35030b8c78ec0bcb5f6f7810145fadbc066baa26
N6REJ <null>
2022-12-09 09:37:23 +0000

    🔄 Synced local 'DF-Zone.lua' with remote 'wow/DF-Zone.lua'

--- DF-Zone.lua
+++ DF-Zone.lua
@@ -1,7 +1,36 @@
----@class ns
+-- AdiBags zone items - Database
+-- Created by @project-author@ character is Bearesquishy - dalaran please credit whenever.
+-- Source on GitHub: https://n6rej.github.io
+---@type
+---
 local addonName, addonTable = ...
 
-addonTable.expansion = {
-  Shadowlands = "Shadowlands",
-  Dragonflight = "Dragonflight"
+-- Get the common name for Release
+local E = addonTable.expansion
+local Expansion = E["Dragonflight"]
+
+-- Create addon table
+local db = {}
+
+-- Filter info
+db.Filters = {
+	["Zone_Reagents"] = {
+		uiName = "Special Reagents",
+		uiDesc = "Special reagents from " .. Expansion,
+		title = "DF Mats",
+		items = {
+			-- ID  = true,		--Item name
+			[190454] = true, -- Primal Chaos
+			[202122] = true, -- Primal Chaos Cluster
+			[200686] = true, -- Primal Focus
+			[190456] = true, -- Artisan's Mettle
+			[201405] = true, -- Tuft of Primal Wool -- Used by many professions
+			[201406] = true, -- Glowing Titan Orb   -- Used by many professions
+			[190453] = true, -- Spark of Ingenuity
+			[199197] = true, -- Bottled Essence
+		}
+	}
 }
+
+-- now that db is populated lets pass it on.
+addonTable.Dragonflight = db
