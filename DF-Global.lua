4a5da552913b6d05eab156885898340f876dbfd1
N6REJ <null>
2022-12-10 11:23:04 +0000

    🔄 Created local 'DF-Global.lua' from remote 'wow/DF-Global.lua'

---
+++ DF-Global.lua
@@ -0,0 +1,47 @@
+-- AdiBags Global items - Database
+-- Created by @project-author@ character is Bearesquishy - dalaran please credit whenever.
+-- Source on GitHub: https://n6rej.github.io
+---@type
+---
+local addonName, addonTable = ...
+
+-- Get the common name for Release
+local E = addonTable.expansion
+local Expansion = E["Dragonflight"]
+
+-- Create addon table
+local db = {}
+
+-- Filter info
+db.Filters = {
+	["Global_Reagents"] = {
+		uiName = "Bears " .. Expansion .. "Global Reagents",
+		uiDesc = "Common reagents from " .. Expansion,
+		title = "DF Mats",
+		items = {
+			-- ID  = true,		--Item name
+			[190454] = true, -- Primal Chaos
+			[202122] = true, -- Primal Chaos Cluster
+			[200686] = true, -- Primal Focus
+			[190456] = true, -- Artisan's Mettle
+			[201405] = true, -- Tuft of Primal Wool -- Used by many professions
+			[201406] = true, -- Glowing Titan Orb   -- Used by many professions
+			[190453] = true, -- Spark of Ingenuity
+			[199197] = true, -- Bottled Essence
+			[194746] = true, -- Thomas's Empty Bottle
+		}
+	},
+	["Special_Reagents"] = {
+		uiName = "Bears " .. Expansion .. "Special Reagents",
+		uiDesc = "Special reagents from " .. Expansion,
+		title = "Special",
+		items = {
+			-- ID  = true,		--Item name
+			[193360] = true, -- Centaur's Trophy Necklace
+			[194746] = true, -- Thomas's Empty Bottle
+		}
+	}
+}
+
+-- now that db is populated lets pass it on.
+addonTable.Dragonflight = db
