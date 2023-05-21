bc5c027dcc3b7d398951d2e1fa7faf3cc29bb751
N6REJ <programming@hallhome.us>
2023-05-21 13:28:39 +0000

    🔄 synced local 'Global-dbs.lua' with remote 'wow/Global-dbs.lua'

--- Global-dbs.lua
+++ Global-dbs.lua
@@ -8,7 +8,7 @@ Put the code below...
 addonTable.DFGlobal = db
 
 at the end of each global database file where the first 2 letters ( DF ) are the expansion and then add that name to the list below.
-The globals should only have the db.Filters not the db.name or db.desc.  See "DF-Global.lua" for an example.
+The globals should only have the db.Filters not the db.name or db.desc.  See "DF-Global.lua" for an example..
 This file should be listed in the .toc just after "expansion.lua"
 
 --]]
