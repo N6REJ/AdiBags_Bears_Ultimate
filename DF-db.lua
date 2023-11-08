71a6c5ddd838a7e9b71064fe42e0f589a9ca9756
Bear <programming@hallhome.us>
2023-11-07 21:51:37 -0600

    removed duplicate Primalist Charm added Plush Pillow

--- DF-db.lua
+++ DF-db.lua
@@ -535,7 +535,7 @@ db.Filters = {
 			[205441] = true, -- Bartered Inscription Journal
 			[204987] = true, -- Lava-Forged Leatherworker's "Knife"
 			[204988] = true, -- Sulfur-soaked skins
-			[198968] = true, -- Primalist Charm
+			[210462] = true, -- Plush Pillow
 		}
 	},
 	["Tools"] = {
